b004e8a62e5df9bf025561f513089ceb438d0bcc
João Paulo <joaopwii@gmail.com>
2025-07-02 10:33:16 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Menu();
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Bem vindo!");
+    Console.WriteLine("[1] Acessar inventário");
+    Console.WriteLine("[2] Acessar lojas");
+    Console.WriteLine("[-1] Sair");
+    Console.WriteLine("Digite uma opção: ");
+    int opcao = int.Parse(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1:
+            //MenuInventario();
+            break;
+        case 2:
+            //MenuLojas();
+            break;
+        case -1:
+            Console.Clear();
+            Console.WriteLine("Até logo!");
+            Console.WriteLine("Saindo...");
+            return;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+}
